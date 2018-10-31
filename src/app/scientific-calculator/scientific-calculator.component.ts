742f8f253edf351d6983c3b39cd1eea9928cfff0
Raul Carrillo <39688592+Mochii967@users.noreply.github.com>
2018-10-30 23:29:36 -0700

    Create scientific-calculator.component.ts

---
+++ src/app/scientific-calculator/scientific-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scientific-calculator',
+  templateUrl: './scientific-calculator.component.html',
+  styleUrls: ['./scientific-calculator.component.css']
+})
+export class ScientificCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
